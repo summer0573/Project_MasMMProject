8a627c2578d6ee974314e9d5bcb98df4175c5922
정현진 <s2113@e-mirim.hs.kr>
2023-02-04 22:35:30 +0900

    코드 정리

--- src/MasProject1/rankingDB.java
+++ src/MasProject1/rankingDB.java
@@ -14,7 +14,6 @@ public class rankingDB {
         String name = "??";
         int score = 6;
 
-        int num = 1;
         try {
 //데이터베이스불러오기
             Connection connection = DriverManager.getConnection(url, userName, password);
@@ -35,12 +34,30 @@ public class rankingDB {
                     break;
             }
 
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        /*
+        int num=1;
+        try {
+//데이터베이스불러오기
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(" INSERT INTO user_scoer_tabel(user_name,user_scoer)  VALUES ('" + name + "', '" + score + "')");//테이블에 스코어를 추가 시키는 큐리문
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user_scoer_tabel ORDER BY user_scoer DESC;");//데이터베이스에서 가져오는 큐리문
+            while(num<=10 && http://rs.next()){//rs 에는 불러온 테이블 내용이 배열 처럼 들어 있으므로 반복문을 이용하여 불러온다
+//num 은 등수 표시하기 위해 만든 거니까 무시해도 된다.
+                scoret+=" "+num++;
+                scoret+= "\t| "+rs.getString(1)+"\t| "+rs.getString(2)+"\n";
+            }
 
         } catch (SQLException e) {
             System.out.println(e);
         }
-//        dialog.setScoreTest(scoret);
-//        dialog.setVisible(true);
+        dialog.setScoreTest(scoret);
+        dialog.setVisible(true);
+    }
+        * */
     }
 
     public static void main(String[] args) throws SQLException {
--- src/MasProject1/rankingShowDB.java
+++ src/MasProject1/rankingShowDB.java
@@ -1,11 +1,17 @@
 package MasProject1;
 
 import java.awt.*;
+import java.sql.SQLException;
 import java.util.List;
 import javax.swing.*;
 
 public class rankingShowDB {
-    public static void main(String[] args) {
+    public rankingShowDB() throws SQLException {
+        rankingDB RDB = new rankingDB();
+    }
+
+
+    public static void main(String[] args) throws SQLException {
 
     }
 
